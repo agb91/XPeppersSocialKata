02cd9b43d81afca764eb51d121252ee6bec1bd0a
agb91 <andrea.damio1991@hotmail.it>
2019-04-25 11:17:47 +0200

    some angular little fixes

--- social-kata-angular-client/src/app/read/read.component.ts
+++ social-kata-angular-client/src/app/read/read.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AjaxService } from '../ajax.service';
+import { isFormattedError, formattedError } from '@angular/compiler';
 
 @Component({
   selector: 'app-read',
@@ -11,6 +12,7 @@ export class ReadComponent implements OnInit {
   sender:string;
   target:string;
   response:string;
+  resultArr:string[];
 
   constructor(private ajax:AjaxService) { }
 
@@ -22,10 +24,27 @@ export class ReadComponent implements OnInit {
   {
     console.log( "read: " + this.sender + "--->" + this.target );
     this.ajax.callRead(this.sender, this.target).subscribe( r => 
-      {this.response = r.response;
-      alert(this.response) 
+      {this.response = this.format(r.response);
     });
 
+  }
+
+  format( str:string ):string
+  {
+    let formatted:string = "";
+    this.resultArr = this.clean( str.split(">") );
+    
+    return formatted;
+  }
+
+  clean( arr:string[] ):string[]
+  {
+
+    alert ( arr );
+      let result:string[] = arr.filter( e => e.trim().length > 0 );
+
+    alert(result);  
+    return result;
   }
 
 }
